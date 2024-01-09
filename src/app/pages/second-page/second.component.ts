6df903a343080ade9d5b5a6dd2a28551d9299567
J <dev.javadashraf@gmail.com>
2024-01-09 13:38:46 +0330

    fix isServer check

--- src/app/pages/second-page/second.component.ts
+++ src/app/pages/second-page/second.component.ts
@@ -1,26 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {  HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, Inject, OnInit } from '@angular/core';
+import {
+  CommonModule,
+  isPlatformBrowser,
+  isPlatformServer,
+} from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { environment } from '../../../environment';
-
+import { PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-second',
   standalone: true,
-  imports: [CommonModule,HttpClientModule],
-  providers:[],
+  imports: [CommonModule, HttpClientModule],
+  providers: [],
   templateUrl: './second.component.html',
-  styleUrl: './second.component.scss'
+  styleUrl: './second.component.scss',
 })
 export class SecondComponent implements OnInit {
   title = 'exon-test';
-  response:any
-  constructor(private http:HttpClient){
-
+  response: any;
+  baseUrl: string = '';
+  constructor(
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) platformId: Object
+  ) {
+    let isBrowser = isPlatformBrowser(platformId);
+    let isServer = isPlatformServer(platformId);
+    this.baseUrl = isBrowser
+      ? 'http://localhost:5000'
+      : isServer
+      ? 'http://api'
+      : 'thanks to j';
   }
   ngOnInit(): void {
-      this.http.get( environment.BASE_URL +'api/test').subscribe((res)=>{
-        this.response = res
-      })
+    this.http.get(this.baseUrl + '/test').subscribe((res) => {
+      this.response = res;
+    });
   }
 }
