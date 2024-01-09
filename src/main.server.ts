46093a66ce3c3476403b052eae809ea73f1a482e
J <dev.javadashraf@gmail.com>
2024-01-09 15:01:09 +0330

    add render application

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
+import { renderApplication } from '@angular/platform-server';
 
 const bootstrap = () => bootstrapApplication(AppComponent, config);
-
-export default bootstrap;
+const output: string = await renderApplication(bootstrap,{});
+export default output;
